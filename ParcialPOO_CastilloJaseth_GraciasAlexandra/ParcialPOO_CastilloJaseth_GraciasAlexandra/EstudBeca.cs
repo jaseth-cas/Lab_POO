aadba15edb457dd96fe141c35abd178913f680a8
jaseth-cas <jaseth.castillo@utp.ac.pa>
2025-10-15 10:41:47 -0500

    Commit para instanciar clases

---
+++ ParcialPOO_CastilloJaseth_GraciasAlexandra/ParcialPOO_CastilloJaseth_GraciasAlexandra/EstudBeca.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParcialPOO_CastilloJaseth_GraciasAlexandra
+{
+    internal class EstudBeca
+    {
+    }
+}
